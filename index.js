8ed0632945da5b01345da622f8911e565439d171
Mizael Paredes Vielma <paredes.mizael89@gmail.com>
2021-05-07 22:24:36 -0400

    Modified

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const activate = () => {
     let minutes = intervalTime.value;
     let hours = totalTime.value;
     motivation.innerText = 'Perfecto, vamos a darle lets go 🚀 ';
-    text.innerText = `¡Te avisaremos cada ${minutes} minutos durante ${hours} horas! 🚀`;
+    text.innerText = `¡Te avisaremos cada ${minutes} minutos durante ${hours} horas! 🚀 ⏰`;
     modal.style.display = 'block';
   }
   setTimer(intervalTime.value);
