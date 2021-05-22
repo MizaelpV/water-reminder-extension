0782bb5cc9a7a0cf76e441a13b627a1f435f17f6
Mizael Paredes Vielma <paredes.mizael89@gmail.com>
2021-05-21 20:37:44 -0400

    modified

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const setTimer = (min) => {
   if (min !== '') {
     const miliseconds = min * 1000;
     setTimeout(() => {
-      console.log('Alert  🔥🔥 ');
+      console.log('Alert 🔥 ');
     }, miliseconds);
   }
 };
