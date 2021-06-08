a54247c155081a8f7e9fea90f1053f5e24331707
Mizael Paredes Vielma <paredes.mizael89@gmail.com>
2021-06-07 23:39:51 -0400

    modified

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const activate = () => {
     motivation.style.backgroundColor = '#f3f3f3';
     motivation.style.color = '#3f2b96';
   } else {
-    motivation.innerText = 'Perfecto, vamos a darle 🚀 ';
+    motivation.innerText = 'Perfecto, vamos a darle 🚀❤️ ';
     text.innerText = `¡Te avisaremos cada ${minutes} minutos durante ${hours} horas!`;
     modal.style.display = 'block';
   }
